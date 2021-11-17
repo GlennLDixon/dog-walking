c451db56e86f6d18609786f3d38391e161d72093
Glenn Dixon Jr <steve@stevenbrownlee.com>
2021-11-16 20:38:45 -0600

    added the clickEventListener to select the right walker for each dog

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,30 @@
 import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
+
+document.addEventListener("click", (clickEvent) => {
+    const itemClicked = clickEvent.target
+    if(itemClicked.id.startsWith("pet")) {
+        const [,petID] = itemClicked.id.split('--')
+
+        for (const pet of pets ) {
+            for (const walker of walkers) {
+                if(pet.id === parseInt(petID) && pet.walkerId === walker.id) {
+                    window.alert(`${pet.name} is being walked by ${walker.name}`)
+                }
+            }
+        }
+    }
+})
 
 const pets = getPets()
+const walkers = getWalkers()
+
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
